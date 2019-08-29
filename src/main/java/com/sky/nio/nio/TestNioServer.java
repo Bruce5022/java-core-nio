6cd33e8e416c5ba169d65856844645e4c78399e3
shizhanwei <shizhanwei123@126.com>
2019-08-29 21:00:04 +0800

    增加服务端代码

--- src/main/java/com/sky/nio/nio/TestNioServer.java
+++ src/main/java/com/sky/nio/nio/TestNioServer.java
@@ -3,9 +3,11 @@ package com.sky.nio.nio;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.*;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
 
 /**
  * 服务端
@@ -34,22 +36,42 @@ public class TestNioServer {
             serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
             // 6.轮训式的获取选择器上已经准备就绪的事件
-            while (selector.select()>0){
+            while (selector.select() > 0) {
 
+                // 7.获取当前选择器上所有注册的"选择键(已就绪的监听事件)"
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    // 8.获取准备"就绪"的事件
+                    SelectionKey selectionKey = iterator.next();
 
+                    // 9.判断具体是什么事件准备就绪
+                    if(selectionKey.isAcceptable()){
+                        // 10.若"接收就绪"，获取客户的连接
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+
+                        // 11.切换非阻塞模式
+                        socketChannel.configureBlocking(false);
+
+                        // 12.将该通道注册到选择器上
+                        socketChannel.register(selector,SelectionKey.OP_READ);
+                    } else if (selectionKey.isReadable()) {
+                        // 13.获取当前选择器上"读就绪"状态的通道
+                        SocketChannel socketChannel = (SocketChannel) selectionKey.channel();
+
+                        // 14.读取数据
+                        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+                        int len;
+                        while ((len = socketChannel.read(byteBuffer)) != -1) {
+                            byteBuffer.flip();
+                            System.out.println(new String(byteBuffer.array(),0,len));
+                            byteBuffer.clear();
+                        }
+                    }
+                    // 15.取消选择键
+                    iterator.remove();
+                }
             }
-            // 3.获取客户端连接的通道
-            SocketChannel socketChannel = serverSocketChannel.accept();
-
-            // 4.分配指定大小的缓冲区
-            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-
-            // 5.接收客户端的数据，并保存到本地
-            while (socketChannel.read(byteBuffer) != -1) {
-                byteBuffer.flip();
-                byteBuffer.clear();
-            }
-
             // 6.关闭通道
         } catch (IOException e) {
             throw e;
